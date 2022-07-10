d94ef76dfd7392716af394db770c2c801797bb19
lovepreet <klovepreet12199@gmail.com>
2022-07-10 15:09:31 -0400

    Initial Commit

---
+++ My First App (Calculator)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My First App (Calculator)
+//
+//  Created by Macbook on 2022-07-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My First App (Calculator)/My_First_App__Calculator_App.swift
@@ -0,0 +1,17 @@
+//
+//  My_First_App__Calculator_App.swift
+//  My First App (Calculator)
+//
+//  Created by Macbook on 2022-07-10.
+//
+
+import SwiftUI
+
+@main
+struct My_First_App__Calculator_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
